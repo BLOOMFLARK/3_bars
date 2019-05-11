58f02085638f4867d3249bb49ffc691b3518405e
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-05-12 00:36:48 +0300

    GOTIT

--- bars.py
+++ bars.py
@@ -2,20 +2,39 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
 def get_biggest_bar(data):
-    pass
+    list_of_seats = [data['features'][i]['properties']['Attributes']['SeatsCount'] for i in range(len(data['features']))]
+    max_seats = max(list_of_seats)
+    index = list_of_seats.index(max_seats)
+
+    return data['features'][index]['properties']['Attributes'].get('Name')
 
 
 def get_smallest_bar(data):
-    pass
+    list_of_seats = [data['features'][i]['properties']['Attributes']['SeatsCount'] for i in
+                     range(len(data['features']))]
+    min_seats = min(list_of_seats)
+    index = list_of_seats.index(min_seats)
+
+    return data['features'][index]['properties']['Attributes'].get('Name')
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    ldata = [data['features'][i]['geometry']['coordinates'] for i in range(len(data['features']))]
+    dist = [(ldata[i][0] - longitude)**2 + (ldata[i][1] - latitude)**2 for i in range(len(ldata))]
+    index = dist.index(min(dist))
+    return data['features'][index]['properties']['Attributes'].get('Name')
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data("/Users/dns/Desktop/yandex/bars.json")
+
+    print(get_biggest_bar(data))
+    print(get_smallest_bar(data))
+    longitude = float(input("longitude :"))
+    latitude = float(input("latitude :"))
+    print(get_closest_bar(data, longitude, latitude))
