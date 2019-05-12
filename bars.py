00841503b20e5c2df9d56f5d1a80bb0f3fa308ea
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-05-12 23:21:49 +0300

    user friendly update

--- bars.py
+++ bars.py
@@ -1,47 +1,50 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
+def load_data(file_path):
+    try:
+        with open(file_path, 'r') as file_handler:
+            return json.load(file_handler)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return None
 
 
-def get_biggest_bar(json_data):
-    list_of_features = json_data['features']
-    list_of_seats = [list_of_features[index]['properties']['Attributes']['SeatsCount'] for index in
-                     range(len(list_of_features))]
-    max_seats = max(list_of_seats)
-    index = list_of_seats.index(max_seats)
-    name_of_bar = list_of_features[index]['properties']['Attributes'].get('Name')
-    return name_of_bar
+def get_name_of_bar(bar_data):
+    return bar_data['properties']['Attributes']['Name']
 
 
-def get_smallest_bar(json_data):
-    list_of_features = json_data['features']
-    list_of_seats = [list_of_features[index]['properties']['Attributes']['SeatsCount'] for index in
-                     range(len(list_of_features))]
-    min_seats = min(list_of_seats)
-    index = list_of_seats.index(min_seats)
-    name_of_bar = list_of_features[index]['properties']['Attributes'].get('Name')
-    return name_of_bar
+def get_biggest_bar(loaded_data):
+    max_seats_bar = max(loaded_data, key=lambda bars: bars['properties']['Attributes']['SeatsCount'])
+    print('biggest bar is ' + get_name_of_bar(max_seats_bar))
 
 
-def get_closest_bar(json_data, longitude, latitude):
-    list_of_features = json_data['features']
-    coordinates = [list_of_features['features'][index]['geometry']['coordinates']
-                   for index in range(len(list_of_features))]
-    distances = [(coordinates[index][0] - longitude)**2 + (coordinates[index][1] - latitude)**2 
-            for index in range(len(coordinates))]
-    index = distances.index(min(dist))
-    name_of_bar = list_of_features[index]['properties']['Attributes'].get('Name')
-    return name_of_bar
+def get_smallest_bar(loaded_data):
+    min_seats_bar = min(loaded_data, key=lambda bars: bars['properties']['Attributes']['SeatsCount'])
+    print('smallest bar is ' + get_name_of_bar(min_seats_bar))
+
+
+def get_closest_bar(loaded_data, longitude, latitude):
+    min_distance_bar = min(loaded_data, key=lambda bars: (bars['geometry']['coordinates'][0] - longitude)**2
+                           + (bars['geometry']['coordinates'][1] - latitude)**2)
+    print('closest bar is ' + get_name_of_bar(min_distance_bar))
 
 
 if __name__ == '__main__':
-    bars_data = load_data("/Users/dns/Desktop/yandex/bars.json")
+    try:
+        file_path = sys.argv[1]
+        bars_data = load_data(file_path)['features']
 
-    print(get_biggest_bar(bars_data))
-    print(get_smallest_bar(bars_data))
-    longitude = float(input("longitude :"))
-    latitude = float(input("latitude :"))
-    print(get_closest_bar(bars_data, longitude, latitude))
+        get_biggest_bar(bars_data)
+        get_smallest_bar(bars_data)
+
+        try:
+            longitude = float(input('longitude :'))
+            latitude = float(input('latitude :'))
+            get_closest_bar(bars_data, longitude, latitude)
+
+        except ValueError:
+            sys.exit('ERROR : incorrect input')
+            
+    except FileNotFoundError:
+        sys.exit('no files to load from')
