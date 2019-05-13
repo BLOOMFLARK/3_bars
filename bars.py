2d310de28979c159e0e295c6ac4c36a53fb82a9c
BLOOMFLARK <50524041+BLOOMFLARK@users.noreply.github.com>
2019-05-13 09:12:23 +0300

    fixed code style

--- bars.py
+++ bars.py
@@ -14,37 +14,48 @@ def get_name_of_bar(bar_data):
     return bar_data['properties']['Attributes']['Name']
 
 
-def get_biggest_bar(loaded_data):
-    max_seats_bar = max(loaded_data, key=lambda bars: bars['properties']['Attributes']['SeatsCount'])
-    print('biggest bar is ' + get_name_of_bar(max_seats_bar))
+def get_biggest_bar(bars_data):
+    max_seats_bar = max(
+        bars_data,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+
+    return get_name_of_bar(max_seats_bar)
 
 
-def get_smallest_bar(loaded_data):
-    min_seats_bar = min(loaded_data, key=lambda bars: bars['properties']['Attributes']['SeatsCount'])
-    print('smallest bar is ' + get_name_of_bar(min_seats_bar))
+def get_smallest_bar(bars_data):
+    min_seats_bar = min(
+        bars_data,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
+    return get_name_of_bar(min_seats_bar)
 
 
-def get_closest_bar(loaded_data, longitude, latitude):
-    min_distance_bar = min(loaded_data, key=lambda bars: (bars['geometry']['coordinates'][0] - longitude)**2
-                           + (bars['geometry']['coordinates'][1] - latitude)**2)
-    print('closest bar is ' + get_name_of_bar(min_distance_bar))
+def get_closest_bar(bars_data, longitude, latitude):
+    min_distance_bar = min(
+        bars_data, key=lambda bar:
+        (bar['geometry']['coordinates'][0] - longitude)**2
+        + (bar['geometry']['coordinates'][1] - latitude)**2)
+
+    return get_name_of_bar(min_distance_bar)
 
 
 if __name__ == '__main__':
-    try:
-        file_path = sys.argv[1]
-        bars_data = load_data(file_path)['features']
 
-        get_biggest_bar(bars_data)
-        get_smallest_bar(bars_data)
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        sys.exit('ERROR : no file path')
+
+    bars_data = load_data(file_path)['features']
+    if not bars_data:
+        sys.exit('ERROR : file not found or file not in a json format')
+    else:
+        print('THE BIGGEST BAR: {bar}'.format(bar=get_biggest_bar(bars_data)))
+        print('THE SMALLEST BAR: {bar}'.format(bar=get_smallest_bar(bars_data)))
 
         try:
             longitude = float(input('longitude :'))
             latitude = float(input('latitude :'))
-            get_closest_bar(bars_data, longitude, latitude)
+            print('THE CLOSEST BAR: {bar}'.format(bar=get_closest_bar(bars_data, longitude, latitude)))
 
         except ValueError:
             sys.exit('ERROR : incorrect input')
-            
-    except FileNotFoundError:
-        sys.exit('no files to load from')
